f07159730cc4aa7c9803f8b44312f66cfaf2afa3
chanalisa <52388966+chanalisa@users.noreply.github.com>
2020-10-09 00:31:56 -0400

    Add basic view for browse endpoint, display NEOs in a list (#1)
    Co-authored-by: Ricky Chon <rickychon99@gmail.com>

---
+++ src/view/Browse/Browse.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+
+import './Browse.css';
+import NeoDetail from './NeoDetail/NeoDetail';
+
+class Browse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      neos: []
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    const {data} = await axios.get(`/api`)
+    this.setState({
+      neos: [...data.neos]
+    })
+  }
+
+  handleClick(event) {
+    event.target.expanded = !event.target.expanded
+  }
+
+  render() {
+    return (
+      <div className='Browse'>
+        <h1>Near Earth Objects</h1>
+        {this.state.neos.map(neo => {
+          return (
+          <div key={neo.id}>
+            <button key={neo.id} className='collapsible' expanded={false} onClick={this.handleClick}>
+              {neo.name}
+            </button>
+            <NeoDetail details={neo} />
+          </div>
+        )})}
+      </div>
+    )
+  }
+}
+
+export default Browse;

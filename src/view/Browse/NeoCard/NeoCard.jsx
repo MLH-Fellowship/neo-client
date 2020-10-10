a0b29c494491802803766acb72b201948d4f38ff
Alisa Chan <alisachan.ny@gmail.com>
2020-10-10 15:38:48 -0400

    2 column neo card

--- src/view/Browse/NeoCard/NeoCard.jsx
+++ src/view/Browse/NeoCard/NeoCard.jsx
@@ -6,11 +6,17 @@ const NeoCard = (props) => {
   return (
     <div className='NeoCard'>
       <div className="contents">
-        ID: {props.details.id}<br />
+        <div className='col-1-of-3'>
+          <img src="../../../../public/assets/images/asteroid-human.png" alt="Asteroid with outline of human overlay"/>
+        </div>
+        <div className='col-2-of-3'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        {/* ID: {props.details.id}<br />
         {props.details.designation}<br />
         <a href={props.details.nasa_jpl_url}>More Info</a><br />
         {props.details.absolute_magnitude_h}<br />
-        {/* Estimated Diameter:<br />
+        Estimated Diameter:<br />
         Kilometers:<br/>
         Minimum: {props.details.estimated_diameter.kilometers.estimated_diameter_min} km<br/>
         Maximum: {props.details.estimated_diameter.kilometers.estimated_diameter_max} km<br/>
@@ -22,9 +28,9 @@ const NeoCard = (props) => {
         Maximum: {props.details.estimated_diameter.miles.estimated_diameter_max} mi<br/>
         Feet:<br/>
         Minimum: {props.details.estimated_diameter.feet.estimated_diameter_min} ft<br/>
-        Maximum: {props.details.estimated_diameter.feet.estimated_diameter_max} ft<br/> */}
+        Maximum: {props.details.estimated_diameter.feet.estimated_diameter_max} ft<br/>
         Potential Hazard: {props.details.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}<br/>
-        Sentry Object: {props.details.is_sentry_object ? 'Yes' : 'No'}
+        Sentry Object: {props.details.is_sentry_object ? 'Yes' : 'No'} */}
       </div>
     </div>
   )

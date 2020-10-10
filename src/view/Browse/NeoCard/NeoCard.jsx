773088c4dc606f52327bc190fd1084b4b6ffbe69
chanalisa <52388966+chanalisa@users.noreply.github.com>
2020-10-10 15:59:21 -0400

    2 column neo card (#10)
    * 2 column neo card
    * Use public url to access image, adjust img size
    
    Co-authored-by: Ricky Chon <rickychon99@gmail.com>

--- src/view/Browse/NeoCard/NeoCard.jsx
+++ src/view/Browse/NeoCard/NeoCard.jsx
@@ -6,11 +6,19 @@ const NeoCard = (props) => {
   return (
     <div className='NeoCard'>
       <div className="contents">
-        ID: {props.details.id}<br />
+        <div className='col-1-of-3'>
+          <img src={process.env.PUBLIC_URL + "/assets/images/asteroid-human.png"} 
+            alt="Asteroid with outline of human overlay"
+            width="100%" height="100%"/>
+        </div>
+        <div className='col-2-of-3'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        {/* ID: {props.details.id}<br />
         {props.details.designation}<br />
         <a href={props.details.nasa_jpl_url}>More Info</a><br />
         {props.details.absolute_magnitude_h}<br />
-        {/* Estimated Diameter:<br />
+        Estimated Diameter:<br />
         Kilometers:<br/>
         Minimum: {props.details.estimated_diameter.kilometers.estimated_diameter_min} km<br/>
         Maximum: {props.details.estimated_diameter.kilometers.estimated_diameter_max} km<br/>
@@ -22,9 +30,9 @@ const NeoCard = (props) => {
         Maximum: {props.details.estimated_diameter.miles.estimated_diameter_max} mi<br/>
         Feet:<br/>
         Minimum: {props.details.estimated_diameter.feet.estimated_diameter_min} ft<br/>
-        Maximum: {props.details.estimated_diameter.feet.estimated_diameter_max} ft<br/> */}
+        Maximum: {props.details.estimated_diameter.feet.estimated_diameter_max} ft<br/>
         Potential Hazard: {props.details.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}<br/>
-        Sentry Object: {props.details.is_sentry_object ? 'Yes' : 'No'}
+        Sentry Object: {props.details.is_sentry_object ? 'Yes' : 'No'} */}
       </div>
     </div>
   )

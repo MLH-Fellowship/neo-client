984fdb266f0df2ece318cf8243ebe2428b754193
Alisa Chan <alisachan.ny@gmail.com>
2020-10-10 19:45:24 -0400

    assign image to asteroid

--- src/view/Browse/NeoCard/NeoCard.jsx
+++ src/view/Browse/NeoCard/NeoCard.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 
 import './NeoCard.scss'
 
+const sizeComparison = {
+  2: 'human',
+  6: 'human-3',
+  10: 'human-5',
+  15: 'bus',
+  45: 'bus-3',
+  50: 'plane',
+  75: 'bus-5',
+  150: 'plane-3',
+  250: 'plane-5',
+};
+
+const sizes = Object.keys(sizeComparison);
+
+const thumbnail = (diameterM) => {
+  let diameter = Math.ceil(diameterM);
+  let i = 0;
+  while (diameter > sizes[i] && i <= sizes.length-1) {
+    i++;
+  }
+  return sizeComparison[sizes[i]];
+};
+
 const NeoCard = (props) => {
   let estDiameter = props.details.estimated_diameter; 
   let diameterM = estDiameter.meters.estimated_diameter_max - estDiameter.meters.estimated_diameter_min;
@@ -11,8 +34,8 @@ const NeoCard = (props) => {
     <div className='NeoCard'>
       <div className="contents">
         <div className='col-1-of-3'>
-          <img src={process.env.PUBLIC_URL + "/assets/images/asteroid-human.png"} 
-            alt="Asteroid with outline of human overlay"
+          <img src={process.env.PUBLIC_URL + `/assets/images/asteroid-${thumbnail(diameterM)}-192.png`} 
+            alt="Asteroid with size comparison overlay"
             width="100%" height="100%"/>
         </div>
         <div className='col-2-of-3'>

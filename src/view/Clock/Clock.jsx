60538233f2bf8366563929e83041090f32c3d85a
Ricky Chon <rickychon99@gmail.com>
2020-10-09 21:03:16 -0400

    Add placeholders for clock values (#8)

--- src/view/Clock/Clock.jsx
+++ src/view/Clock/Clock.jsx
@@ -4,8 +4,8 @@ const Clock = (props) =>
 {
     return (
         <div>
-            <h3>{props.time}</h3>
-            <h5>{props.date}</h5>
+            <h3>{props.time ? props.time : "00:00:00"}</h3>
+            <h5>{props.date ? props.date : "Loading..."}</h5>
         </div>
     );
 };

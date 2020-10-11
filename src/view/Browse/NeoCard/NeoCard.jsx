3e4a8d4340dd420bc601a95d58369ec1f32077ae
Ricky Chon <rickychon99@gmail.com>
2020-10-10 21:08:28 -0400

    Update NeoCard styling and class names (#15)

--- src/view/Browse/NeoCard/NeoCard.jsx
+++ src/view/Browse/NeoCard/NeoCard.jsx
@@ -3,26 +3,30 @@ import React from 'react';
 import './NeoCard.scss'
 
 const NeoCard = (props) => {
-  let estDiameter = props.details.estimated_diameter; 
-  let diameterM = estDiameter.meters.estimated_diameter_max - estDiameter.meters.estimated_diameter_min;
-  let diameterF = estDiameter.feet.estimated_diameter_max - estDiameter.feet.estimated_diameter_min;
+  let neoInfo = props.details;
+  let estDiameter = neoInfo.estimated_diameter; 
+  let diameterM = (estDiameter.meters.estimated_diameter_max + estDiameter.meters.estimated_diameter_min) / 2;
+  let diameterF = (estDiameter.feet.estimated_diameter_max + estDiameter.feet.estimated_diameter_min) / 2;
 
   return (
-    <div className='NeoCard'>
-      <div className="contents">
-        <div className='col-1-of-3'>
-          <img src={process.env.PUBLIC_URL + "/assets/images/asteroid-human.png"} 
-            alt="Asteroid with outline of human overlay"
-            width="100%" height="100%"/>
+    <div className="neo-card">
+      <div className='col-1-of-3'>
+        <img src={process.env.PUBLIC_URL + "/assets/images/asteroid-human.png"} 
+          className="neo-card-photo"
+          alt="Asteroid with outline of human overlay"
+          width="100%"/>
+      </div>
+      <div className='col-2-of-3'>
+        <div className="neo-card-title">
+          <p><strong>Name:</strong> {neoInfo.name}</p>
+          <p><strong>ID:</strong> {neoInfo.id}</p>
         </div>
-        <div className='col-2-of-3'>
-          <p>Name: {props.details.name}</p>
-          <p>ID: {props.details.id}</p>
-          <br/>
-          <p>Estimated Diameter:</p>
+        <br/>
+        <div className="neo-card-details">
+          <p><strong>Estimated Diameter:</strong></p>
           <p>{diameterM.toFixed(2)}m | {diameterF.toFixed(2)}ft</p>
           <br/>
-          <p>Potential Hazard: {props.details.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
+          <p><strong>Potential Hazard:</strong> {neoInfo.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
         </div>
       </div>
     </div>

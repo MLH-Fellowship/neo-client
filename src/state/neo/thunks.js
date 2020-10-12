84fb81ca416683aac48df2dd88c1cac1ea7aff8c
Ricky Chon <rickychon99@gmail.com>
2020-10-12 07:21:23 -0400

    React app api url

--- src/state/neo/thunks.js
+++ src/state/neo/thunks.js
@@ -5,14 +5,14 @@ import { fetchBrowsedNeos, fetchBrowsedNeosReverse } from "./actions";
 // THUNK CREATORS
 export const fetchBrowsedNeosThunk = (page) => (dispatch) =>
 {
-    return axios.get(`/api/browse?page=${page}`)
+    return axios.get(`${process.env.REACT_APP_API_URL}/api/browse?page=${page}`)
     .then((res) => dispatch(fetchBrowsedNeos(res.data.neos)))
     .catch((err) => console.error(err));
 };
 
 export const fetchBrowsedNeosReverseThunk = (page) => (dispatch) =>
 {
-    return axios.get(`/api/browse?page=${page}`)
+    return axios.get(`${process.env.REACT_APP_API_URL}/api/browse?page=${page}`)
     .then((res) => dispatch(fetchBrowsedNeosReverse(res.data.neos)))
     .catch((err) => console.error(err));
 };
--- src/state/news/thunks.js
+++ src/state/news/thunks.js
@@ -5,7 +5,7 @@ import { fetchNews } from "./actions";
 // THUNK CREATORS
 export const fetchNewsThunk = () => (dispatch) =>
 {
-    return axios.get(`/api/news`)
+    return axios.get(`${process.env.REACT_APP_API_URL}/api/news`)
     .then((res) => dispatch(fetchNews(res.data.news)))
     .catch((err) => console.error(err));
 };

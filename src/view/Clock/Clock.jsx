32c77eb1ba5d93ab2590c7a47ad1d7b825512c3d
Alisa Chan <alisachan.ny@gmail.com>
2020-10-11 23:28:30 -0400

    switched neo data

--- src/view/Clock/Clock.jsx
+++ src/view/Clock/Clock.jsx
@@ -4,8 +4,8 @@ const Clock = (props) =>
 {
     return (
         <div>
-            <h3>{props.time ? props.time : "00:00:00"}</h3>
-            <h5>{props.date ? props.date : "Loading..."}</h5>
+            <h3 className='content-section-header'>{props.time ? props.time : "00:00:00"}</h3>
+            <h5 className='content-section-header'>{props.date ? props.date : "Loading..."}</h5>
         </div>
     );
 };

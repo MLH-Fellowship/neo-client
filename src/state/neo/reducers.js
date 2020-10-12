5650392062f894fafb2f51eb3d05a8dc737e2675
Ricky Chon <rickychon99@gmail.com>
2020-10-11 21:57:23 -0400

    Update bi-directional infinite scrolling
    * Now working properly, top and bottom pages will be kept track of,
    and their respective prev/next pages will load

--- src/state/neo/reducers.js
+++ src/state/neo/reducers.js
@@ -3,7 +3,9 @@ import types from "./action_types";
 const initialState =
 {
     neos: [],
-    page: {}
+    page: {},
+    topPage: {},
+    bottomPage: {}
 };
 
 // REDUCER
@@ -15,12 +17,14 @@ const reducer = (state = initialState, action) =>
             return {
                 ...state,
                 neos: [...state.neos, ...action.payload.near_earth_objects],
+                bottomPage: action.payload.page,
                 page: action.payload.page
             };
         case types.FETCH_BROWSED_NEOS_REVERSE:
             return {
                 ...state,
                 neos: [...action.payload.near_earth_objects, ...state.neos],
+                topPage: action.payload.page,
                 page: action.payload.page
             };
         default:

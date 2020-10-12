73d2850b6e668a8e80feb41c4aed28259281f623
Ricky Chon <rickychon99@gmail.com>
2020-10-12 05:03:52 -0400

    Updates

---
+++ precache-manifest.b9a50cf0bf37dce09b24bdc7febbf932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e98c44fd3a7c29408639a87f9bd30b",
+    "url": "/neo-client/index.html"
+  },
+  {
+    "revision": "60d75ae54f87e36a3f0b",
+    "url": "/neo-client/static/css/main.40c83008.chunk.css"
+  },
+  {
+    "revision": "b4073ffff66954a65e0d",
+    "url": "/neo-client/static/js/2.823cf3f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/neo-client/static/js/2.823cf3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d75ae54f87e36a3f0b",
+    "url": "/neo-client/static/js/main.5767e6c6.chunk.js"
+  },
+  {
+    "revision": "4a679eef0826928d5cc8",
+    "url": "/neo-client/static/js/runtime-main.670a3ab4.js"
+  }
+]);

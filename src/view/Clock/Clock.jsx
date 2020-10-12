7de96045a034b2638ca7fc5d4eb02e9b3aa7df4a
chanalisa <52388966+chanalisa@users.noreply.github.com>
2020-10-12 01:37:53 -0400

    aesthetics (#30)

--- src/view/Clock/Clock.jsx
+++ src/view/Clock/Clock.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './Clock.scss';
+
 const Clock = (props) =>
 {
     return (
-        <div>
-            <h3 className='content-section-header'>{props.time ? props.time : "00:00:00"}</h3>
-            <h5 className='content-section-header'>{props.date ? props.date : "Loading..."}</h5>
+        <div className='Clock'>
+            <h3 className='content-section-header time'>{props.time ? props.time : "00:00:00"}</h3>
+            <h5 className='content-section-header date'>{props.date ? props.date : "Loading..."}</h5>
         </div>
     );
 };
